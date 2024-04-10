b26b86d839d0b6aeafe3fe36bf9523414c82d710
Kriish sharma <kriish.sharma2006@gmail.com>
2024-04-10 13:55:22 +0530

    changes

--- src2/tbscalc.cpp
+++ src2/tbscalc.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include "hello.h"
 #define DMRSREsinaPRB 12
 #define DMRSREinaPRBsymbol2  24
 #define NRB_SC 12
@@ -45,7 +44,7 @@ public:
         std::cout << "Number of symbols of the PDSCH allocation within the slot: ";
         std::cin >> Nsh_Sym;
         
-// DMRS CONFIG
+        // DMRS CONFIG
 
         std::cout << "DMRS Single or Double symbol: ";
         std::cin >> DMRS_SYM;
@@ -64,7 +63,7 @@ public:
         float N_info = NRE * Code_Rate * Modulation_order * numlayers;
 
         std::cout << "Intermediate number of information bits are " << N_info << std::endl;
-
+        float TBS ;
         if (N_info <= 3824) {
             int n = std::max(3, static_cast<int>(std::log2(N_info)) - 6);
             int N_info_quant_1 = std::max(24, static_cast<int>(std::pow(2, n)) * static_cast<int>(std::floor(N_info / std::pow(2, n))));
@@ -73,6 +72,25 @@ public:
             int n = static_cast<int>(std::log2(N_info - 24)) - 5;
             int N_info_quant = static_cast<int>(std::pow(2, n)) * static_cast<int>(std::round((N_info - 24) / std::pow(2, n)));
             std::cout << "Quantized intermediate number of information bits: " << N_info_quant << std::endl;
+
+            if (Code_Rate <= 0.25) {
+                std::cout << "\ncode rate <= 0.25\n";
+                int C = ceil((N_info_quant + 24) / 3816.0);
+                TBS = 8 * C * ceil((N_info_quant + 24) / (8 * C)) - 24;
+                std::cout << "\nTransport Block Size: " <<TBS<<" bits";
+            } else {
+                std::cout << "\ncode rate > 0.25\n";
+                if (N_info_quant >= 8424) {
+                    std::cout << "\nQuantized intermediate number of information bits >= 8424\n"; 
+                    float C = ceil((N_info_quant + 24) / 8424.0);
+                    TBS = 8 * C * ceil((N_info_quant + 24) / (8 * C)) - 24;
+                    std::cout << "\nTransport Block Size: " <<TBS<<" bits";
+                } else {
+                    std::cout << "\nQuantized intermediate number of information bits < 8424\n";
+                    TBS = 8 * ceil((N_info_quant + 24) / 8) - 24;
+                    std::cout << "\nTransport Block Size: " <<TBS<<"bits";
+                }
+            }
         }
     }
 
@@ -106,16 +124,16 @@ private:
     
     void getports(int numlayers) {
         if (numlayers > 4) {
-            std::cout<<"WRONG NUMBER OF LAYERS FOR CW0";
+            std::cout << "WRONG NUMBER OF LAYERS FOR CW0";
             exit(1);
         }
         int port = 1000;
-        std::cout<<"PORTS : ";
+        std::cout << "PORTS : ";
         for (int i = 0 ; i < numlayers ; i++){
-            std::cout<<port<<" ";
+            std::cout << port << " ";
             port++;
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
     
     int getdmrsre(int symbols , int addpos){
@@ -129,12 +147,9 @@ private:
     }
 };
 
-
-
 int main() {
-    hello();
     PDSCHCalculator calculator;
     calculator.calculatePDSCH();
     return 0;
-}
+} 
 
